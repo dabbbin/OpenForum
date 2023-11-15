16444fa1c5e717ac74340029019c38ee2142d069
bboooyaho <rhkahrake100@naver.com>
2023-11-15 22:14:11 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,7 @@
 from .base import * 
 
 ALLOWED_HOSTS = ['43.202.81.190']
+#base_dir : /home/ubuntu/projects/mysite
+STATIC_ROOT = BASE_DIR / 'static/'
+#base.py staticfiles_dirs 와 중복 오류를 막기 위해 
+STATICFILES_DIRS = []
